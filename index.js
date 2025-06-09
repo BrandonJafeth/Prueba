ef820cabbeb552497b409af39eb515688ec99912
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2025-06-09 02:48:41 -0600

    feat/ cambios

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 import { gsap } from "https://cdn.skypack.dev/gsap";
-
-console.log("GSAP is ready to use!");
-
+import { SplitText } from "https://cdn.skypack.dev/gsap/SplitText";
 
 
-let tl = gsap.timeline();
+gsap.registerPlugin(SplitText)
 
 
-tl.to("#Yellow", {
+const splitText = new SplitText(".text", {
+    type: "words"
+});
+
+
+gsap.from(splitText.words, {
     duration: 1,
-    repeat: -1, 
-    x: 100,
-    rotation: 360,
-    yoyo: true,
-})
-tl.to("#Red", {
-    duration: 1,
-    repeat: -1,
-    x: 100,
-    rotation: 360,
-    yoyo: true,
-}) 
+    y: 10, // estado inicial 
+    stagger : 0.1, // segundos entre cada palabra
+    autoAlpha: 0,
+    filter: "blur(10px)",
+});
+
+
+// let tl = gsap.timeline();
+
+
+// tl.to("#Yellow", {
+//     duration: 3,
+//     x: 900,
+//     y : -100,
+ 
+// })
+// tl.to("#Red", {
+//     duration: 3,
+//   x: 500,
+// }) 
+
+// tl.to("#Blue", {
+//     duration: 3,
+//     x: 900,
+  
+// }, "<");
 
